2fa1381210a72b7d7e175170811fe24437c2e121
Mert Demirok <mertdemirok80@gmail.com>
2019-12-07 15:35:45 +0100

    validate an URL

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,12 @@
-const validator = require('validator');
+const validator = require('validator'); // No-ES6
+// import validator from 'validator'; // ES6
 const getNotes = require('./notes');
 
 const msg = getNotes();
 
-console.log(validator.isEmail('mertdemirok80@gmail.com'));
+console.log(validator.isURL('https://mead.io'));
+// console.log(validator.isEmail('mertdemirok80@gmail.com'));
+
 
 
 
