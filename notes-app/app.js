dd4c46a3eaaea2e4217dccdcf6ed4e319d5dd74d
Mert Demirok <mertdemirok80@gmail.com>
2019-12-08 00:46:45 +0100

    deleted the redundant codes

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,36 +10,7 @@ const msg = getNotes();
 
 const log = console.log;
 // Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-// Compose multiple styles using the chainable API
-log(chalk.blue.bgRed.bold('Hello world!'));
-// Pass in multiple arguments
-log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
-// Nest styles
-log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
-// Nest styles of the same type even (color, underline, background)
-log(chalk.green(
-    'I am a green line ' +
-    chalk.blue.underline.bold('with a blue substring') +
-    ' that becomes green again!'
-));
-//define your own themes
-const error = chalk.bold.red;
-const warning = chalk.keyword('orange');
- 
-console.log(error('Error!'));
-console.log(warning('Warning!'));
-
-// Take advantage of console.log string substitution:
-
-const name = 'Sindre';
-console.log(chalk.green('Hello %s'), name);
-//=> 'Hello Sindre'
-
-
-
-// console.log(validator.isURL('https://mead.io'));
-// console.log(validator.isEmail('mertdemirok80@gmail.com'));
+log(chalk.blue('Hello') + ' World' + chalk.blue('!'));
 
 
 
