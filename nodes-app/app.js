7645943cda2ce90cfc9f50d2da6a51143dbf4912
Mert Demirok <mertdemirok80@gmail.com>
2019-12-07 10:16:45 +0100

    calling another js file

--- nodes-app/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// create a text file and write a message inside
-fs.writeFileSync('note.txt', 'My name is Mert.');
-
-// append a message to the existing message
-fs.appendFileSync('note.txt', ' I live in Belgium');
-fs.appendFileSync('note.txt', '.');
-
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
+require('./utils');
 
-// create a text file and write a message inside
-fs.writeFileSync('note.txt', 'My name is Mert.');
+const name = 'Mert';
 
-// append a message to the existing message
-fs.appendFileSync('note.txt', ' I live in Belgium');
-fs.appendFileSync('note.txt', '.');
+console.log(name);
+
+
 
