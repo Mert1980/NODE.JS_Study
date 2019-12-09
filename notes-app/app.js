122c13cb9c2056259389c5c44e1b3a355c2a4e31
Mert Demirok <mertdemirok80@gmail.com>
2019-12-09 13:59:12 +0100

    redundant codes removed from app.js, json files committed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,11 @@
-// Challenge: Use the chalk library in your project
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const msg = getNotes();
+console.log(msg);
 
-// console.log(chalk.green('Success!'));
-// console.log(chalk.bgBlue.bold('Hello World!'));
-// console.log(chalk.inverse.bold('Hello World!'));
-
-const log = console.log;
-// Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.blue('!'));
+const greenMsg = chalk.blue.bold.inverse('Success!');
+console.log(greenMsg);
 
 
 
