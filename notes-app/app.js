91861638c63f393990cf3c35ac7d027654e33853
Mert Demirok <mertdemirok80@gmail.com>
2019-12-09 14:18:46 +0100

    writing messages to the console using process.argv property

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,10 @@
-const chalk = require('chalk');
-const getNotes = require('./notes');
+const chalk = require("chalk");
+const getNotes = require("./notes");
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-const greenMsg = chalk.blue.bold.inverse('Success!');
-console.log(greenMsg);
-
-console.log(process.argv[2]);
-
-
-
-
-
-
-
-
-
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === 'remove') {
+    console.log("Removing note!");
+}
