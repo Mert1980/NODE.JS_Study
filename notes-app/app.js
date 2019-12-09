51bbf56b6e5e79ca0483155b3da3ca9766593432
Mert Demirok <mertdemirok80@gmail.com>
2019-12-09 14:59:19 +0100

    create add command

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,6 +5,17 @@ const getNotes = require("./notes");
 // Customize yargs version
 yargs.version('1.1.0');
 
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note');
+    }
+})
+
+// add, remove, read, list
+
 console.log(yargs.argv);
 
 
