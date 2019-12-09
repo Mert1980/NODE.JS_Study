da21015133b0b6f5eea998e094a99a3371f7fb87
Mert Demirok <mertdemirok80@gmail.com>
2019-12-09 15:04:22 +0100

    Create remove command

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,20 +3,26 @@ const yargs = require("yargs");
 const getNotes = require("./notes");
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version("1.1.0");
 
 // Create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note');
-    }
-})
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note");
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Removing a note",
+  handler: function() {
+    console.log("Removong a note");
+  }
+});
 
 // add, remove, read, list
 
 console.log(yargs.argv);
-
-
- 
