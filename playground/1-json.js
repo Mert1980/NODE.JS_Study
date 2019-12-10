58c7b3f7768a73c376dd8d569705c01c5f0c7515
Mert Demirok <mertdemirok80@gmail.com>
2019-12-10 12:28:18 +0100

    read a file, convert it  to JSON string and parse it asJS object

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
-const book = {
-  title: "De Vriendschap",
-  author: "Deam Koontz"
-};
+// const book = {
+//   title: "De Vriendschap",
+//   author: "Deam Koontz"
+// };
 
-const bookJson = JSON.stringify(book);
-fs.writeFileSync("1-json.json", bookJson);
+// const bookJson = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJson); // Create a new JSON file
+
+const dataBuffer = fs.readFileSync('1-json.json') // returns the data in binary
+const dataJSON = dataBuffer.toString(); // returns JSON string
+const data = JSON.parse(dataJSON); // returns data in object format
+console.log(data);
+
