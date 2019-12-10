6e4db74c6d6bf4eb9e0d722204de0ca12bf79c79
Mert Demirok <mertdemirok80@gmail.com>
2019-12-10 12:11:25 +0100

    create a new JSON file and store book data

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,9 @@
+const fs = require("fs");
+
 const book = {
-    title: 'De Vriendschap',
-    author: 'Deam Koontz'
-}
+  title: "De Vriendschap",
+  author: "Deam Koontz"
+};
 
 const bookJson = JSON.stringify(book);
-// console.log(bookJson);
-
-const parsedData = JSON.parse(bookJson);
-
-console.log(parsedData.author);
+fs.writeFileSync("1-json.json", bookJson);
