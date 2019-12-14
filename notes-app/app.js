34208e288dbb1b6dee6541abe331b2799a146a43
Mert Demirok <mertdemirok80@gmail.com>
2019-12-14 14:48:37 +0100

    exporting two functions

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 // Customize yargs version
 yargs.version("1.1.0");
