6a1f05a80965cf97bb2b53bd8515b9124c22f605
Mert Demirok <mertdemirok80@gmail.com>
2019-12-14 18:19:04 +0100

    avoid duplication of titles with array filter method

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,11 +6,21 @@ const getNotes = function() {
 
 const addNote = function(title, body) {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body
+
+  const dublicateNotes = notes.filter(note => {
+    return note.title === title;
   });
-  saveNotes(notes);
+
+  if (dublicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
 };
 
 const saveNotes = function(notes) {
