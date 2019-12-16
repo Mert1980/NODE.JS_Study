a117acb144721cf96f33012849dc91673b05a401
Mert Demirok <mertdemirok80@gmail.com>
2019-12-16 23:26:34 +0100

    references folder deleted

--- reference/fs-demo.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-//Create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => { //by default "fs.mkdir" is async
-  if (err) throw err;
-  console.log("Folder created...");
-});
